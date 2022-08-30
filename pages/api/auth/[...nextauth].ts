20b9519cbb2e35cdfbd1ed9582d11383be6c1436
Dzakirz <dzakirz.dev@gmail.com>
2022-08-30 19:28:26 +0700

    upload

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,23 @@
+// pages/api/auth/[...nextauth].ts
+
+import { NextApiHandler } from "next"
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options)
+export default authHandler
+
+const options = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    })
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET
+}
